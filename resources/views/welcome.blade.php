759ec8709c97bcd63074ccbf9a08967cddb7c179
Karboon01 <81914223+Karboon01@users.noreply.github.com>
2024-07-04 13:56:42 +0400

    Добавлены миграции для создания таблиц users и tasks

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,6 @@
                         <div class="flex flex-col gap-2">
                             <h1 class="text-3xl">{{ $task->name }}</h1>
                             <p class="text-xl">{{ $task->descr }}</p>
-
                         </div>
                         <form action="{{ route('deleteTask', ['id' => $task->id]) }}"
                             class="flex items-center justify-between" method="post">
@@ -44,8 +43,9 @@
                 </div>
                 <div class="mb-5">
                     <label for="descr" class="block mb-2 text font-medium text-while">Описание</label>
-                    <input id="descr" type="text" name="descr" required
-                        class="bg-neutral-700 border border-neutral-600 text-white text rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 placeholder-neutral-400" />
+                    <textarea id="descr" type="text" name="descr" required 
+                        class="bg-neutral-700 border border-neutral-600 text-white text rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 placeholder-neutral-400 resize-none"
+                        cols="30" rows="10"></textarea>
                 </div>
                 <button type="submit"
                     class="text-xl bg-neutral-600 px-4 py-2 rounded border-2 border-neutral-400 hover:bg-neutral-500 duration-150 ease-in-out">
